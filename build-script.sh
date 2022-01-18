75697649af52ac9b95fa83502d489b4894feb5f7
Jonny Le <jonnyle2@gmail.com>
2022-01-18 14:46:23 -0600

    feature kubernetes

--- build-script.sh
+++ build-script.sh
@@ -7,4 +7,4 @@ export VAULT_TOKEN=$(curl -X POST -d "{\"role\": \"sops\", \"jwt\": \"$(cat $JWT
 sops -d --hc-vault-transit ${VAULT_ADDR}/v1/sops/keys/first-key encrypted.ini > config.ini
 
 # start api
-uvicorn main:app --host 0.0.0.0 --port 80
+uvicorn main:app --host 0.0.0.0 --port 80 --root-path /xsautomate
