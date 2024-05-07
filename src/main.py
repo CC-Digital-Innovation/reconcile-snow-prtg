5e1105169a1b6b01f1057dc1f12f24bcb483ef7c
Jonny Le <jonnyle2@gmail.com>
2024-05-07 13:29:50 -0500

    wip: reuse CI model; TODO clean up PRTG controller

--- src/main.py
+++ src/main.py
@@ -24,7 +24,7 @@ from alt_prtg import PrtgController
 from report import get_add_device_model
 from snow import ApiClient as SnowClient
 from snow import SnowController, get_prtg_tree_adapter
-from snow.models.record import SnowData
+from snow.models import CIBody
 
 from sync import sync_trees, RootMismatchException
 
@@ -301,35 +301,25 @@ def sync_all_sites(company_name: str = Form(..., description='Name of Company'),
         raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, 'An unexpected error occurred.')
     return f'Successfully added {len(devices_added)} devices to {company_name}.'
 
-
-
-
-        
 @logger.catch
 @app.post("/sync_device")
-def sync_device(snow_data: SnowData):
-
-
-        try:
-            auth = BasicToken(snow_data.prtg_api_key)
-            client = PrtgClient(f'https://{snow_data.prtg_url}', auth, requests_verify=True)
-            prtg_controller = PrtgController(client)
-        except Exception:
-            logger.error(f"Error creating PRTG client")
-            return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Error creating PRTG client")
-        
+def sync_device(ci_body: CIBody):
+        auth = BasicToken(ci_body.prtg_api_key)
+        client = PrtgClient(f'https://{ci_body.prtg_url}', auth, requests_verify=True)
+        prtg_controller = PrtgController(client)
         try:
             # Get parent group of device for company brackets
-            device = client.get_device(snow_data.objid)
+            device = client.get_device(ci_body.ci.id)
             parent_group = client.get_group(device['parentid'])
             company_brackets = parent_group["name"].split(" ")[0]
+            logger.debug(company_brackets)
 
             # Get the grandparent group
             grandparent_group = client.get_group(parent_group['parentid'])
         
             # Check if the used_for group exists
-            grandparent_used_for = company_brackets + " " + snow_data.used_for
-            category_group = company_brackets + " " + snow_data.category
+            grandparent_used_for = company_brackets + " " + ci_body.ci.stage
+            category_group = company_brackets + " " + ci_body.ci.category
 
             try:
                 used_for_group = prtg_controller.get_group_existence(grandparent_group["parentid"], grandparent_used_for)
@@ -338,7 +328,7 @@ def sync_device(snow_data: SnowData):
                 # Move the device to the category group in the used_for group and set it's properties
                 grandparent_group = client.add_group(grandparent_used_for, grandparent_group['parentid'])
                 category_group = client.add_group(category_group, grandparent_group['objid'])
-                prtg_controller.moveobj_setproperties_deleteobj(snow_data, category_group['objid'], parent_group)
+                prtg_controller.moveobj_setproperties_deleteobj(ci_body, category_group['objid'], parent_group)
                 return f"{grandparent_group['name']}, {category_group['name']} created; Device moved to {category_group['name']} within {grandparent_group['name']}"
             
             # Since the grandparent group exists, check if the category group exists within the grandparent group
@@ -348,18 +338,16 @@ def sync_device(snow_data: SnowData):
                 logger.info(f"Creating category group instead :{e}")
                 # Move the device to the category group and set it's properties
                 category_group = client.add_group(category_group, used_for_group['objid'])
-                prtg_controller.moveobj_setproperties_deleteobj(snow_data, category_group['objid'], parent_group)
+                prtg_controller.moveobj_setproperties_deleteobj(ci_body, category_group['objid'], parent_group)
                 return f"{used_for_group['name']} exists; {category_group['name']} created; Device moved to {category_group['name']} within {used_for_group['name']}"
             
             # Since the category group exists within the existing grandparent group, move the device to the category group and set it's properties
             try:
-                prtg_controller.moveobj_setproperties_deleteobj(snow_data, category_group['objid'], parent_group)
+                prtg_controller.moveobj_setproperties_deleteobj(ci_body, category_group['objid'], parent_group)
                 return f"{category_group['name']}, {used_for_group['name']} exist; Device moved to {used_for_group['name']} within {category_group['name']}"
             except Exception as e:
                 logger.error(f"Unexpected Error: {e}")
                 raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, 'An unexpected error occurred.')
-
-            
         except HTTPException as e:
             # Reraise already handled exception
             logger.error(e)
--- src/snow/models/cmdb_ci.py
+++ src/snow/models/cmdb_ci.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 from ipaddress import IPv4Address
 from typing import Union
 
+from pydantic import BaseModel
+
 
 @dataclass
 class Manufacturer:
@@ -21,3 +23,12 @@ class ConfigItem:
     link: str
     prtg_id: Union[int, None]
     is_internal: bool
+
+class CIBody(BaseModel):
+    """Model for API endpoint body to sync a configuration item from 
+    ServiceNow."""
+    # PRTG
+    prtg_url: str
+    prtg_api_key: str
+
+    ci: ConfigItem
