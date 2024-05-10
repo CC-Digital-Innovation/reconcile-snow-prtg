7e9abb5ebf45909df7b820cc9f2672ec2d6df269
Jonny Le <jonnyle2@gmail.com>
2024-05-10 15:08:44 -0500

    revert to old union annotation for compatibilty with pydantic

--- src/snow/models/cmdb_ci.py
+++ src/snow/models/cmdb_ci.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from ipaddress import IPv4Address
+from typing import Union
 
 from pydantic import BaseModel
 
@@ -12,24 +13,25 @@ class Manufacturer:
 
 @dataclass(slots=True)
 class ConfigItem:
+    """Dataclass for ServiceNow configuration items. The attributes uses older
+    Union annotation for compatibility with pydantic."""
     id: str
     name: str
-    ip_address: IPv4Address | None
-    manufacturer: Manufacturer | None
+    ip_address: Union[IPv4Address, None]
+    manufacturer: Union[Manufacturer, None]
     model_number: str
     stage: str
     category: str
     sys_class: str
     link: str
-    prtg_id: int | None
+    prtg_id: Union[int, None]
     is_internal: bool
-    host_name: str | None = None
+    host_name: Union[str, None] = None
 
 class CIBody(BaseModel):
-    """Model for API endpoint body to sync a configuration item from 
-    ServiceNow."""
-    # PRTG
+    """Model for API endpoint body to sync a configuration item from ServiceNow."""
     prtg_url: str
     prtg_api_key: str
-
+    company_name: str
+    location_name: str
     ci: ConfigItem
