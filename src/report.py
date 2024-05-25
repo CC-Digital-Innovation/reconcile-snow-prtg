658fc21c4c27e83867034cc1142bb7046e7338d1
Jonny Le <jonnyle2@gmail.com>
2024-05-24 23:24:06 -0500

    feature delete table and namedtuple

--- src/report.py
+++ src/report.py
@@ -1,15 +1,24 @@
-from dataclasses import dataclass
+from typing import NamedTuple
 
 from prtg import ApiClient as PrtgClient
 
 from alt_prtg.models import Device
 
 
-@dataclass
-class AddedDeviceModel:
+class AddedDeviceModel(NamedTuple):
     name: str
     link: str
     service_url: str
 
-def get_add_device_model(device: Device, api_client: PrtgClient):
-    return AddedDeviceModel(device.name, api_client.device_url(device.id), device.service_url)
+    @classmethod
+    def from_device(cls, device: Device, api_client: PrtgClient):
+        return cls(device.name, api_client.device_url(device.id), device.service_url)
+
+
+class DeletedDeviceModel(NamedTuple):
+    name: str
+    service_url: str
+
+    @classmethod
+    def from_device(cls, device: Device):
+        return cls(device.name, device.service_url)
