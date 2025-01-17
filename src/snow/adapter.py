6e449cb690646bf06efd3c02654fad8771c8759d
Jonny Le <jonnyle2@gmail.com>
2025-01-17 00:19:25 -0500

    refactor tree adapter and feature sys_class subgroup (#45)
    * refactor tree adapter

--- src/snow/adapter.py
+++ src/snow/adapter.py
@@ -1,16 +1,15 @@
-from collections import defaultdict
+from abc import ABC, abstractmethod
+from collections.abc import Iterable, Mapping, Set
 from dataclasses import dataclass, fields
-from functools import reduce, partial
-from operator import getitem
+from functools import reduce
 from string import Formatter
 
 from loguru import logger
 from prtg import Icon
 
-from .models import ConfigItem, Company, Location
-from alt_prtg.models import Device, Group, Status, Node
+from alt_prtg.models import Device, Group, Node, Status
 from snow import SnowController
-
+from snow.models import Company, ConfigItem, Location
 
 # map SNOW choice list names to formats
 FORMAT_MAP = {
@@ -78,18 +77,160 @@ class PrtgGroupAdapter(Group):
         super(PrtgGroupAdapter, self).__init__(None, name, 3, set(), '', Status.UP, True)
 
 
-def _build_pseudo_tree(depth: int):
-    """Recursive function that dynamically builds psuedo tree depth with leaf nodes containing a list.
-    
-    E.g., build_tree(2) allows a defaultdict like:
-        {
-            group-1: {
-                group-2: []
-            }
-        }
-    """
-    factory = partial(_build_pseudo_tree, depth - 1) if depth > 1 else list
-    return defaultdict(factory)
+class FieldGroup(ABC):
+    """ConfigItems are organized by its field values, so FieldGroups are rules 
+    to structure the tree at each level. A FieldGroup defines a function 
+    get_group to determine which group it belongs to and the next FieldGroup to 
+    follow."""
+    @abstractmethod
+    def get_group(self, ci:ConfigItem) -> tuple[str, Node]:
+        pass
+
+
+class ValueFieldGroup(FieldGroup):
+    """Represents a group organized by the value itself."""
+    def __init__(self, field_name: str, child: FieldGroup | None = None, children_map: Mapping[str, FieldGroup] | None = None):
+        """
+        Args:
+            field_name(str): name of attribute
+            child(FieldGroup | None): next FieldGroup to follow. Cannot be set alongside children_map. Defaults to None.
+            children_map(Mapping[str, FieldGroup] | None): mapping of next FieldGroup depending on field value.
+                Cannot be set alongside child. Defaults to None.
+        """
+        self.field_name = field_name
+        self.child = child
+        self.children_map = children_map
+
+    def get_group(self, ci: ConfigItem):
+        value = getattr(ci, self.field_name)
+        child = self.child  # default to child attribute
+        if self.children_map:
+            try:
+                child = self.children_map[value]  # update only if children_map was set
+            except KeyError:
+                pass
+        return (value, child)
+
+
+class BooleanFieldGroup(FieldGroup):
+    """Represents a group organized by a boolean field."""
+    def __init__(self,
+                 field_name: str,
+                 t_group_name: str,
+                 f_group_name: str,
+                 t_child: FieldGroup | None = None,
+                 f_child: FieldGroup | None = None):
+        """
+        Args:
+            field_name(str): name of attribute
+            t_group_name(str): name of group if attribute value is True
+            f_group_name(str): name of group if attribute value is False
+            t_child(FieldGroup | None): next FieldGroup if value is True. Defaults to None.
+            f_child(FieldGroup | None): next FieldGroup if value is False. Defaults to None.
+        """
+        self.field_name = field_name
+        self.t_group_name = t_group_name
+        self.f_group_name = f_group_name
+        self.t_child = t_child
+        self.f_child = f_child
+
+    def get_group(self, ci: ConfigItem):
+        value = getattr(ci, self.field_name)
+        return (self.t_group_name, self.t_child) if value else (self.f_group_name, self.f_child)
+
+
+class ChoiceSetFieldGroup(FieldGroup):
+    """Represents a group organized by a choice set field."""
+    def __init__(self, 
+                 field_name: str,
+                 choices: Set[str],
+                 group_name: str,
+                 child: FieldGroup | None = None,
+                 children_map: Mapping[str, FieldGroup] | None = None):
+        """
+        Args:
+            field_name(str): name of attribute
+            choices(Set[str]): set of choices
+            group_name(str): name of group if attribute value is one of the choices
+            child(FieldGroup | None): next FieldGroup to follow. Cannot be set alongside children_map. Defaults to None.
+            children_map(Mapping[str, FieldGroup] | None): mapping of next FieldGroup depending on field value.
+                Cannot be set alongside child. Defaults to None.
+        """
+        self.field_name = field_name
+        self.choices = choices
+        self.group_name = group_name
+        if child and children_map:
+            raise ValueError('child and children_map attributes are mutually exclusive.')
+        self.child = child
+        self.children_map = children_map
+
+    def get_group(self, ci: ConfigItem):
+        """
+        Returns:
+            str: when attribute value is in choices
+            None: when attribute value is not in choices
+        """
+        value = getattr(ci, self.field_name)
+        child = self.child  # default to child attribute
+        if self.children_map:
+            try:
+                child = self.children_map[value]  # update only if children_map was set
+            except KeyError:
+                pass
+        return (self.group_name, child) if value in self.choices else (None, child)
+
+
+class MultiChoiceSetFieldGroup(FieldGroup):
+    """Represents a group organized by multiple choice set field groups."""
+    def __init__(self, choiceset_field_groups: Iterable[ChoiceSetFieldGroup]):
+        """
+        Args:
+            choiceset_field_groups(Iterable[ChoiceSetFieldGroup]): list of ChoiceSetFieldGroups
+        """
+        self.choiceset_field_groups = choiceset_field_groups
+
+    @classmethod
+    def from_dict(cls, field_name: str, field_groups_dict: Mapping[str, Iterable[str]]):
+        return cls((ChoiceSetFieldGroup(field_name, set(choices), group_name) for group_name, choices in field_groups_dict.items()))
+
+    def get_group(self, ci: ConfigItem):
+        """
+        Returns:
+            str: when attribute value is in choices
+            None: when attribute value is not in choices
+        """
+        for field_group in self.choiceset_field_groups:
+            group_name, child = field_group.get_group(ci)
+            if group_name is not None:
+                return (group_name, child)
+        return (None, None)  # no matches found
+
+
+class TreeBuilder:
+    def __init__(self, root: Node, levels: FieldGroup, group_name_fmt: str | None = None, device_name_fmt_key: str | None = None):
+        self.root = root
+        self.levels = levels
+        self.group_name_fmt = group_name_fmt
+        self.device_name_fmt_key = device_name_fmt_key
+
+    def add_ci(self, ci: ConfigItem):
+        parent = self.root  # track parent in loop
+        current_level = self.levels # track current level
+        while current_level:
+            group_name, current_level = current_level.get_group(ci)  # get name and next level
+            if group_name is None:
+                # no matches for group, simply add to current group
+                break
+            if self.group_name_fmt is not None:
+                group_name = self.group_name_fmt.format(group_name)  # format group name
+            try:
+                next_subgroup = next(node for node in parent.children if node.prtg_obj.name == group_name)
+            except StopIteration:
+                # subgroup does not exist, create it
+                next_subgroup = Node(PrtgGroupAdapter(group_name), parent=parent)
+            
+            parent = next_subgroup  # update current parent
+        return Node(PrtgDeviceAdapter.from_ci(ci, self.device_name_fmt_key), parent=parent)
 
 
 # Tree creation is more complex so avoided a class object
@@ -128,48 +269,59 @@ def get_prtg_tree_adapter(company: Company,
 
     # Enough devices to organize into groups
 
-    # Tuple of attributes used for grouping
-    # Order matters: left to right represents going deeper into the tree
-    filters = ('is_internal', 'stage', 'category')
+    # organize subgroups by sys_class_name
+    # keys are subgroup names and values are list of matching sys_class_names
+    server_class_map = {
+        'Out of Band Management': ['Out of Band Management'],
+        'Windows Server': ['Windows Server'],
+        'Linux Server': ['Linux Server'],
+        'Application Server': ['Application Server']
+    }
 
-    # build temporary tree
-    pseudo_tree = _build_pseudo_tree(len(filters))
+    network_class_map = {
+        'IP Switch': ['Switch', 'IP Switch', 'IP Switch Cluster'],
+        'APs': ['Wireless Access Point'],
+        'IP Router': ['IP Router', 'Router', 'Internet Gateway'],
+        'Firewall': ['Firewall Manager', 'Firewall Hardware', 'Software Firewall', 'Firewall Device']
+    }
 
-    # Add ci to psuedo tree as leaf node
+    storage_class_map = {
+        'Storage Device': ['Storage Device'],
+        'Storage Controller': ['Storage Controller'],
+        'Storage Cluster': ['Storage Cluster'],
+        'Storage Node': ['Storage Node'],
+        'Storage Switch': ['Storage Switch']
+    }
+
+    virt_class_map = {
+        'vCenter': ['VMware vCenter Instance'],
+        'Hypervisor': ['ESX Server', 'Hyper-V Server'],
+        'Virtual Machine': ['Virtual Machine Instance', 'VMware Virtual Machine Instance', 'Windows Server', 'Linux Server', 'MS SQL DataBase', 'Application Server'],
+    }
+
+    hardware_class_map = {
+        'PDU': ['PDU'],
+        'UPS': ['UPS']
+    }
+
+    # create field groups for organizing tree. Build in reverse order to attach child nodes
+    server_class_group = MultiChoiceSetFieldGroup.from_dict('sys_class', server_class_map)
+    network_class_group = MultiChoiceSetFieldGroup.from_dict('sys_class', network_class_map)
+    storage_class_group = MultiChoiceSetFieldGroup.from_dict('sys_class', storage_class_map)
+    virt_class_group = MultiChoiceSetFieldGroup.from_dict('sys_class', virt_class_map)
+    hardware_class_group = MultiChoiceSetFieldGroup.from_dict('sys_class', hardware_class_map)
+    category_map = {
+        'Server': server_class_group,
+        'Network': network_class_group,
+        'Storage': storage_class_group,
+        'Virtualization': virt_class_group,
+        'Hardware': hardware_class_group
+    }
+    category_group = ValueFieldGroup('category', children_map=category_map)
+    stage_group = ValueFieldGroup('stage', child=category_group)
+    is_internal_group = BooleanFieldGroup('is_internal', 'CC Infrastructure', 'Customer Managed Infrastructure', f_child=stage_group)
+
+    tree_builder = TreeBuilder(site, is_internal_group, group_name_fmt, company.prtg_device_name_format)
     for ci in config_items:
-        # Grab group names
-        filter_values = [getattr(ci, attr) for attr in filters]
-        reduce(getitem, filter_values[:-1], pseudo_tree)[filter_values[-1]].append(ci)
-
-    # Build actually tree from pseudo tree
-    # First group is based on bool attribute is_internal
-    if True in pseudo_tree:
-        # Internal devices
-        internal_node = Node(PrtgGroupAdapter(group_name_fmt.format('CC Infrastructure')), parent=site)
-        # No more groups required for internal devices, simply add devices
-        for _, categories in pseudo_tree[True].items():
-            for _, cis in categories.items():
-                for ci in cis:
-                    Node(PrtgDeviceAdapter.from_ci(ci, company.prtg_device_name_format), parent=internal_node)
-    if False in pseudo_tree:
-        # Customer managed devices
-        external_node = Node(PrtgGroupAdapter(group_name_fmt.format('Customer Managed Infrastructure')), parent=site)
-        for stage, categories in pseudo_tree[False].items():
-            stage_node = Node(PrtgGroupAdapter(group_name_fmt.format(stage)), parent=external_node)
-            for category, cis in categories.items():
-                cat_node = Node(PrtgGroupAdapter(group_name_fmt.format(category)), parent=stage_node)
-                # specific case for Access Points
-                if category == 'Network':
-                    ap_node = Node(PrtgGroupAdapter(group_name_fmt.format('APs')), parent=cat_node)
-                    for ci in cis:
-                        if ci.sys_class == 'Wireless Access Point':
-                            Node(PrtgDeviceAdapter.from_ci(ci, company.prtg_device_name_format), parent=ap_node)
-                        else:
-                            Node(PrtgDeviceAdapter.from_ci(ci, company.prtg_device_name_format), parent=cat_node)
-                    if not ap_node.children:
-                        # no children means group is not necessary, remove from tree
-                        ap_node.parent = None
-                    continue
-                for ci in cis:
-                    Node(PrtgDeviceAdapter.from_ci(ci, company.prtg_device_name_format), parent=cat_node)
+        tree_builder.add_ci(ci)
     return root
